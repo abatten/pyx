3d171a227bf79343598c521458b5487dff7da9d7
abatten <adamjbatten@gmail.com>
2019-09-11 14:42:36 +1000

    Added math tools tests

--- pyx/__init__.py
+++ pyx/__init__.py
@@ -2,3 +2,5 @@ from .params import Params
 from . import math_tools
 from . import print_tools
 from . import str_tools
+
+from .__version__ import __version__
--- tests/test_math_tools.py
+++ tests/test_math_tools.py
@@ -10,3 +10,24 @@ def test_z_to_cMpc_zero():
     # Zero redshift should give zero distance
     dist = 0.0
     assert pyxcosmo.z_to_cMpc(0).value == dist
+
+def test_z_to_cMpc_array():
+    # Must return an array
+    dist = np.array([0.0, 3395.905416665515])
+    assert (pyxcosmo.z_to_cMpc(np.array([0, 1])).value == dist).all
+
+def test_cMpc_to_z_float():
+    z = 0.8479314667609102
+    assert pyxcosmo.cMpc_to_z(3000) == z
+
+def test_z_to_cMpc_zero():
+    # Zero distance should give zero redshift
+    z = 0.0
+    assert pyxcosmo.cMpc_to_z(0) == z
+
+def test_z_to_cMpc_array():
+    # Must return an array
+    z = np.array([0.0, 0.8479314667609102])
+    assert (pyxcosmo.z_to_cMpc(np.array([0.0, 3000])).value == z).all
+
+
