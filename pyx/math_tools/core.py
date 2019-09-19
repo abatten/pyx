034ecc7f68e33874c43983cb9383a7d704d11284
abatten <adamjbatten@gmail.com>
2019-09-19 10:55:32 +1000

    Added mean_median_mode function

--- pyx/math_tools/core.py
+++ pyx/math_tools/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 
 def reshape_to_1D(array):
     """
@@ -22,3 +23,19 @@ def reshape_to_1D(array):
 
     return array.reshape(1, total_elements)
 
+def mean_median_mode(array):
+    """
+    Returns the mean, median and mode of an array
+
+    Parameters
+    ----------
+    array: numpy.ndarray
+
+    Returns:
+    mean, median, mod
+    """
+    mean = np.mean(array)
+    median = np.median(array)
+    mode = stats.mode(array)[0][0]
+
+    return mean, median, mode
