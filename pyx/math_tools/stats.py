22efda8051b6b7f61815a6048473a3987fe4c5ad
abatten <adamjbatten@gmail.com>
2020-05-06 15:00:50 +1000

    Added gaussian to pyx.math_tools.stats

--- pyx/math_tools/stats.py
+++ pyx/math_tools/stats.py
@@ -18,3 +18,41 @@ def mean_median_mode(array):
     mode = stats.mode(array)[0][0]
 
     return mean, median, mode
+
+
+def gaussian(x, mu, sig, scale=None, normed=False):
+    """
+    Generates a gaussian curve 
+
+    Parameters:
+    x : numpy.ndarray
+        x values
+
+    mu : float or int
+        The mean of the gaussian curve.
+
+    sig : float or int
+        The standard deviation of the gaussian curve.
+
+    scale : float or int
+
+    normed : boolean, optional
+        If normed is true, the gassuan returned will be normalised such
+        that the area under the curve intergrates to unity.
+        Setting `normed = True` overrides the value in scale.
+        Default: False
+
+    """
+    if scale is None:
+        a = 1
+
+    else:
+        a = scale
+
+    if normed:
+        a = 1 / (sig * np.sqrt(2 * np.pi))
+
+    return a * np.exp(-(x - mu)**2.0 / (2 * sig**2.0))
+
+
+
