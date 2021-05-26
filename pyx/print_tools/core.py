19b870cc7b5e86ea8dc01be61bdc11e74c9a9c82
abatten <adamjbatten@gmail.com>
2021-05-26 11:58:17 +1000

    Deleted make_vprint from print_tools

--- pyx/print_tools/core.py
+++ pyx/print_tools/core.py
@@ -11,30 +11,3 @@ def vprint(*args, verbose=True, **kwargs):
     if verbose:
         print(*args, **kwargs)
 
-
-def _do_nothing(*args, **kwargs):
-    pass
-
-
-def make_vprint(verbose=True):
-    """
-    Make a togglable print function.
-
-    If verbose is true, make_vprint will return the print function. If 
-    false, then make_vprint will return a function that does nothing.
-
-    Examples
-    --------
-    >>> vprint = make_vprint(verbose=True)
-    >>> vprint("Example Text")
-    "Example Text"
-
-    >>> vprint = make_vprint(verbose=False)
-    >>> vprint("Example Text")
-
-    """
-
-    if verbose:
-        return print
-    else:
-        return donothing
